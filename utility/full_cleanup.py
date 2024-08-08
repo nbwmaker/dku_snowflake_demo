842788921a26081c8615dd8b3308ff9cf0977118
Nick <nicholas.b.wanamaker@gmail.com>
2024-08-08 16:47:14 -0400

    Updating file extension to match file type

--- utility/full_cleanup.py
+++ utility/full_cleanup.py
@@ -30,38 +30,38 @@ def connect() -> snowflake.connector.SnowflakeConnection:
 
 conn = connect()
 
--- conn.cursor().execute("")
+# conn.cursor().execute("")
 conn.cursor().execute("USE ACCOUNTADMIN")
 
--- Kaggle Databases ===================================================================
--- reset demo env
--- DROP DATABASE IF EXISTS DKU_DEMO_DEV;
--- DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.DEV_CHANGE_HISTORY;
+# Kaggle Databases ===================================================================
+# reset demo env
+# DROP DATABASE IF EXISTS DKU_DEMO_DEV;
+# DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.DEV_CHANGE_HISTORY;
 
 conn.cursor().execute("DROP DATABASE IF EXISTS DKU_DEMO_DEV")
 conn.cursor().execute("DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.DEV_CHANGE_HISTORY")
 
--- reset staging env
--- DROP DATABASE IF EXISTS DKU_DEMO_STAGING;
--- DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.STAGING_CHANGE_HISTORY;
+# reset staging env
+# DROP DATABASE IF EXISTS DKU_DEMO_STAGING;
+# DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.STAGING_CHANGE_HISTORY;
 
 conn.cursor().execute("DROP DATABASE IF EXISTS DKU_DEMO_STAGING")
 conn.cursor().execute("DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.STAGING_CHANGE_HISTORY")
 
 
--- reset prod env
--- DROP DATABASE IF EXISTS DKU_DEMO_PROD;
--- DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.PROD_CHANGE_HISTORY;
+# reset prod env
+# DROP DATABASE IF EXISTS DKU_DEMO_PROD;
+# DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.PROD_CHANGE_HISTORY;
 
 conn.cursor().execute("DROP DATABASE IF EXISTS DKU_DEMO_PROD")
 conn.cursor().execute("DROP TABLE IF EXISTS CENTRAL_DB.SCHEMACHANGE.PROD_CHANGE_HISTORY")
 
 
--- Kaggle RAP Demo Roles ===================================================================
--- DROP ROLE IF EXISTS kaggle_role;
--- DROP ROLE IF EXISTS nintendo;
--- DROP ROLE IF EXISTS sony;
--- DROP ROLE IF EXISTS microsoft;
+# Kaggle RAP Demo Roles ===================================================================
+# DROP ROLE IF EXISTS kaggle_role;
+# DROP ROLE IF EXISTS nintendo;
+# DROP ROLE IF EXISTS sony;
+# DROP ROLE IF EXISTS microsoft;
 
 conn.cursor().execute("DROP ROLE IF EXISTS kaggle_role")
 conn.cursor().execute("DROP ROLE IF EXISTS nintendo")
@@ -69,30 +69,30 @@ conn.cursor().execute("DROP ROLE IF EXISTS sony")
 conn.cursor().execute("DROP ROLE IF EXISTS microsoft")
 
 
--- Flask API ===================================================================
--- stopping the API
--- USE ROLE DATA_API_ROLE;
--- ALTER SERVICE API.PUBLIC.API SUSPEND;
+# Flask API ===================================================================
+# stopping the API
+# USE ROLE DATA_API_ROLE;
+# ALTER SERVICE API.PUBLIC.API SUSPEND;
 
--- -- view status of API service
--- SHOW SERVICES IN COMPUTE POOL API;
+# # view status of API service
+# SHOW SERVICES IN COMPUTE POOL API;
 
--- dropping all objects related to the API
--- USE ACCOUNTADMIN;
--- DROP SERVICE IF EXISTS API.PUBLIC.API;
--- DROP DATABASE IF EXISTS API;
--- DROP ROLE IF EXISTS DATA_API_ROLE;
--- DROP COMPUTE POOL IF EXISTS API;
--- DROP WAREHOUSE IF EXISTS DATA_API_WH;
+# dropping all objects related to the API
+# USE ACCOUNTADMIN;
+# DROP SERVICE IF EXISTS API.PUBLIC.API;
+# DROP DATABASE IF EXISTS API;
+# DROP ROLE IF EXISTS DATA_API_ROLE;
+# DROP COMPUTE POOL IF EXISTS API;
+# DROP WAREHOUSE IF EXISTS DATA_API_WH;
 
 conn.cursor().execute("DROP SERVICE IF EXISTS API.PUBLIC.API")
 conn.cursor().execute("DROP DATABASE IF EXISTS API")
 conn.cursor().execute("DROP ROLE IF EXISTS DATA_API_ROLE")
 conn.cursor().execute("DROP COMPUTE POOL IF EXISTS API")
 
--- Central resources ===================================================================
--- DROP DATABASE IF EXISTS CENTRAL_DB;
--- DROP WAREHOUSE IF EXISTS XSMALL_WH;
+# Central resources ===================================================================
+# DROP DATABASE IF EXISTS CENTRAL_DB;
+# DROP WAREHOUSE IF EXISTS XSMALL_WH;
 
 conn.cursor().execute("DROP DATABASE IF EXISTS CENTRAL_DB")
 conn.cursor().execute("DROP WAREHOUSE IF EXISTS XSMALL_WH")
